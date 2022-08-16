2fcf28667b99ac66d2b2e7d85c2c6697d1653964
irishandyesiam <nathan.irish@gmail.com>
2022-08-16 14:52:27 -0500

    add basic classes

---
+++ HvsAI.py
@@ -0,0 +1,7 @@
+from player import Player
+
+class AI(Player):
+    def __init__(self) -> None:
+        super().__init__()
+
+ai_player = Player("Siri")
---
+++ HvsH.py
@@ -0,0 +1,8 @@
+from player import Player
+
+class Human(Player):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+human_player = Player("Chester")
