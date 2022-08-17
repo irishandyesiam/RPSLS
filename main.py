ee6771c63dc16791dc86ed2d52078534673010ef
Jalen Arrington <jmarrington178@yahoo.com>
2022-08-17 14:06:50 -0400

    test

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 ##run game##
-# from HvsH import Human
+from HvsH import Human
 from HvsAI import AI
+from game import Game
 
-game = AI()
+game = Game()
+human = Human()
 
-game.select_gesture()
+game.run_game()
+
+# human.select_gesture()
 # game.compare()
