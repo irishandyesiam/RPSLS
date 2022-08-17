ab7e8f543de03860dc2d15f6da2d7e1558363774
irishandyesiam <nathan.irish@gmail.com>
2022-08-17 11:26:33 -0500

    Test run compare. Success!

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 ##run game##
 # from HvsH import Human
 from HvsAI import AI
+from game import Game
 
-game = AI()
 
-game.select_gesture()
+# game = AI()
+game = Game()
+
+game.run_game()
+
+
 # game.compare()
